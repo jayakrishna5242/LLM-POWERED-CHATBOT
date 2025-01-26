9ce743396967cfa9abea6501c42b789188e33b8f
jayakrishna5242 <133623469+jayakrishna5242@users.noreply.github.com>
2025-01-26 12:40:41 +0530

    Add files via upload

---
+++ LLM CHATBOT.py
@@ -0,0 +1,32 @@
+import ollama
+
+def chat_with_llama3():
+    print("************************")
+    print("Welcome to llama3 chatbot")
+    print("Enter 'exit' to leave the chat")
+    print("************************",end="")
+    while True:
+        user_input = input("\nYou: ")
+        if user_input.lower() == 'exit':
+            print("Ending chat. Goodbye!")
+            break
+        llama_response = send_to_llama3(user_input)
+        print("Llama-3: " + llama_response)
+
+def send_to_llama3(user_input):
+    llama_response = ollama.chat(
+    model="llama3",
+    messages=[
+        {
+            "role": "user",
+            "content": user_input,
+        },
+    ],
+    )
+  
+    return llama_response["message"]["content"]
+
+# Start the chat
+chat_with_llama3()
+
+ 
